f7c6f43fd5cbd5a99d67c54a951da44af27bbafc
wikim <wikim@markany.com>
2020-06-05 13:47:30 +0900

    1. code_main.c  + add main( ) function
    2. Makefile
     + build script with code_main.c file

---
+++ user/code_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(
+    int                         argc__,
+    char                        **argv__
+    )
+{
+    printf( "Hello openCode\n" );
+
+    return 0;
+}
