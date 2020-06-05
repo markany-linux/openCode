b092849e304e49a487d726baa57d1939fb58c024
wikim <wikim@markany.com>
2020-06-05 10:35:53 +0900

    1. Makefile  + module skeleton code build
    2. mod_main.c
     + module skeleton code

---
+++ kernel/mod_main.c
@@ -0,0 +1,23 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+
+static int __init hello_markany_openCode( void )
+{
+    printk( "%s: Hello Kernel!\n", __FUNCTION__ );
+    return 0;
+}
+
+
+static void __exit bye_markany_openCode( void )
+{
+    printk( "%s: Bye Kernel\n", __FUNCTION__ );
+}
+
+
+module_init( hello_markany_openCode );
+module_exit( exit_markany_openCode );
+
+MODULE_LICENSE( "GPL" );
+MODULE_AUTHOR( "MarkAny Inc." );
+MODULE_DESCRIPTION( "MarkAny openCode Module" );
