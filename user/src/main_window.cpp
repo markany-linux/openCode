bae624b325c70130a218be5d6453a2308e6c7259
MaBling <swma@markany.com>
2020-08-27 16:25:58 +0900

    * user/ 1. main_window.cpp/h  [ + ] 종료 버튼 추가  [ * ] 메인 박스에 파생 위젯들을 추가하는 코드 함수로 분리
    2. main.cpp
     [ + ] 종료 버튼으로 앱을 종료하기 위해 app 포인터 메인 윈도우로 전달

--- user/src/main_window.cpp
+++ user/src/main_window.cpp
@@ -5,21 +5,22 @@
 
 MainWindow::MainWindow(
 	const std::string& subtitle__,
-	const std::vector<ButtonInfo>& button_list__
+	const std::vector<ButtonInfo>& button_list__,
+	const Glib::RefPtr<Gtk::Application> app__
 	)
-		: main_box_(Gtk::ORIENTATION_VERTICAL),
-		  subtitle_(subtitle__), button_box_(button_list__)
+		: app_( app__ ),
+		  main_box_( Gtk::ORIENTATION_VERTICAL ),
+		  subtitle_( subtitle__ ), button_box_( button_list__ ),
+		  quit_box_( ), quit_button_( kQuitButtonText )
+		 
 {
 	std::cout << "[+] MainWindow::MainWindow()\n";
 	/// 사용자가 임의로 종료를 시도할 때에 대한 핸들러 등록
 	signal_delete_event( )
 		.connect( sigc::mem_fun( *this, &MainWindow::on_delete_event ) );
-	
-	/// 상단 제목 라벨 메인 박스에 추가
-	main_box_.pack_start( subtitle_ );
-	/// 버튼 박스 메인 박스에 추가
-	main_box_.pack_start( button_box_ );
 
+	/// 메인 박스에 파생 위젯들을 attach
+	AttachChildWidgets( );
 	/// 메인 박스를 메인 윈도우에 추가
 	add( main_box_ );
 	
@@ -33,6 +34,31 @@ MainWindow::~MainWindow( )
 	std::cout << "MainWindow::~MainWindow()\n";
 }
 
+void MainWindow::AttachChildWidgets( )
+{
+	std::cout << "[+] MainWindow::AttachChildWidgets()\n";
+	/// 상단 제목 라벨 메인 박스에 추가
+	main_box_.pack_start( subtitle_ );
+	/// 버튼 박스 메인 박스에 추가
+	main_box_.pack_start( button_box_ );
+
+	AttachQuitButton( );
+	std::cout << "[-] MainWindow::AttachChildWidgets()\n";
+}
+
+void MainWindow::AttachQuitButton( )
+{
+	std::cout << "[+] MainWindow::AttachQuitButton()\n";
+	quit_button_.signal_clicked( )
+		.connect( sigc::mem_fun( *this,
+								 &MainWindow::on_quit_button_clicked ) );
+	/// 박스에 종료 버튼을 붙임
+	quit_box_.pack_start( quit_button_, Gtk::PACK_EXPAND_PADDING );
+	/// 메인 박스에 종료 버튼이 담긴 박스를 붙임
+	main_box_.pack_start( quit_box_, Gtk::PACK_SHRINK );
+	std::cout << "[-] MainWindow::AttachQuitButton()\n";
+}
+
 bool MainWindow::on_delete_event(
 	GdkEventAny* any_event__
 	)
@@ -43,4 +69,13 @@ bool MainWindow::on_delete_event(
 	std::cout << "[-] MainWindow::on_delete_event()\n";
 	/// 종료 명령 무시
 	return true;
+}
+
+void MainWindow::on_quit_button_clicked( )
+{
+	std::cout << "[+] MainWindow::on_quit_button_clicked()\n";
+	std::cout << "Bye bye\n";
+	/// 프로그램 종료
+	app_->quit( );
+	std::cout << "[-] MainWindow::on_quit_button_clicked()\n";
 }
