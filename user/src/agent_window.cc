271f63abf6245b7b37ec6c879ef74c136d6c401e
MaBling <swma@markany.com>
2020-09-02 15:38:01 +0900

    * user/ 1. agent_window.cc/h  [ * ] MainWindow 클래스명을 AgentWindow로 변경

--- user/src/agent_window.cc
+++ user/src/agent_window.cc
@@ -1,10 +1,11 @@
-#include "main_window.h"
-#include "gtkmm/box.h"
+#include "agent_window.h"
 
 #include <iostream>
 #include <tuple>
 
-MainWindow::MainWindow(
+#include <gtkmm/box.h>
+
+AgentWindow::AgentWindow(
 	const std::string& subtitle__,
 	const std::vector<ButtonInfo>& button_list__
 	)
@@ -13,10 +14,10 @@ MainWindow::MainWindow(
 		  quit_box_( ), quit_button_( kQuitButtonText )
 		 
 {
-	std::cout << "[+] MainWindow::MainWindow()\n";
+	std::cout << "[+] AgentWindow::AgentWindow()\n";
 	/// 사용자가 임의로 종료를 시도할 때에 대한 핸들러 등록
 	signal_delete_event( )
-		.connect( sigc::mem_fun( *this, &MainWindow::on_delete_event ) );
+		.connect( sigc::mem_fun( *this, &AgentWindow::on_delete_event ) );
 
 	/// 메인 박스에 파생 위젯들을 attach
 	AttachChildWidgets( );
@@ -31,58 +32,59 @@ MainWindow::MainWindow(
 	
 	/// 모든 객체 보여주기
 	show_all_children( );
-	std::cout << "[-] MainWindow::MainWindow()\n";
+	std::cout << "[-] AgentWindow::AgentWindow()\n";
 }
 
-MainWindow::~MainWindow( )
+AgentWindow::~AgentWindow( )
 {
-	std::cout << "MainWindow::~MainWindow()\n";
+	std::cout << "AgentWindow::~AgentWindow()\n";
 }
 
-void MainWindow::AttachChildWidgets( )
+void AgentWindow::AttachChildWidgets( )
 {
-	std::cout << "[+] MainWindow::AttachChildWidgets()\n";
+	std::cout << "[+] AgentWindow::AttachChildWidgets()\n";
 	/// 상단 제목 라벨 메인 박스에 추가
 	main_box_.pack_start( subtitle_box_, Gtk::PACK_SHRINK, 20 );
 	/// 버튼 박스 메인 박스에 추가
 	main_box_.pack_start( contents_box_ );
 	/// 종료 버튼 메인 박스에 추가
 	AttachQuitButton( );
-	std::cout << "[-] MainWindow::AttachChildWidgets()\n";
+	std::cout << "[-] AgentWindow::AttachChildWidgets()\n";
 }
 
-void MainWindow::AttachQuitButton( )
+void AgentWindow::AttachQuitButton( )
 {
-	std::cout << "[+] MainWindow::AttachQuitButton()\n";
+	std::cout << "[+] AgentWindow::AttachQuitButton()\n";
 	/// 종료 버튼 클릭 이벤트에 핸들러 등록
 	quit_button_.signal_clicked( )
 				.connect(
 					sigc::mem_fun( *this,
-								   &MainWindow::on_quit_button_clicked ) );
+								   &AgentWindow::on_quit_button_clicked ) );
 	/// 박스에 종료 버튼을 붙임
 	quit_box_.pack_start( quit_button_, Gtk::PACK_EXPAND_PADDING );
 	/// 메인 박스에 종료 버튼이 담긴 박스를 붙임
 	main_box_.pack_start( quit_box_, Gtk::PACK_SHRINK, 20 );
-	std::cout << "[-] MainWindow::AttachQuitButton()\n";
+	std::cout << "[-] AgentWindow::AttachQuitButton()\n";
 }
 
-bool MainWindow::on_delete_event(
+bool AgentWindow::on_delete_event(
 	GdkEventAny* any_event__
 	)
 {
-	std::cout << "[+] MainWindow::on_delete_event()\n";
+	std::cout << "[+] AgentWindow::on_delete_event()\n";
 	/// any_event는 아직 꼭 사용해야 하는 것은 아님.
 	std::ignore = any_event__;
-	std::cout << "[-] MainWindow::on_delete_event()\n";
+	std::cout << "[-] AgentWindow::on_delete_event()\n";
 	/// 종료 명령 무시
 	return true;
 }
 
-void MainWindow::on_quit_button_clicked( )
+void AgentWindow::on_quit_button_clicked( )
 {
-	std::cout << "[+] MainWindow::on_quit_button_clicked()\n";
+	std::cout << "[+] AgentWindow::on_quit_button_clicked()\n";
 	std::cout << "Bye bye\n";
 	/// 프로그램 종료
 	hide( );
-	std::cout << "[-] MainWindow::on_quit_button_clicked()\n";
+	std::cout << "[-] AgentWindow::on_quit_button_clicked()\n";
 }
+
