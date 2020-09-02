fbc615c57101d9fe41449ab5452789b0015f56d1
MaBling <swma@markany.com>
2020-09-02 17:18:29 +0900

    * user/ 1. agent_main.cc/h  [ + ] AgentWindow 사용자 임의 종료 처리
    2. agent_window.cc/h
     [ - ] AgentWindow 사용자 임의 종료 처리를 AgentMain에게 위임

--- user/src/agent_main.cc
+++ user/src/agent_main.cc
@@ -1,7 +1,16 @@
 #include "agent_main.h"
 
+#include <iostream>
+
 AgentMain::AgentMain( )
 {
+	/// 애플리케이션이 구동을 시작할 때에 대한 핸들러 등록
+	app_->signal_startup( ).connect(
+		sigc::mem_fun( *this, &AgentMain::on_startup ) );
+
+	/// 사용자가 임의로 종료를 시도할 때에 대한 핸들러 등록
+	agent_window_.signal_delete_event( )
+		.connect( sigc::mem_fun( *this, &AgentMain::on_delete_event ) );
 }
 
 AgentMain::~AgentMain( )
@@ -10,14 +19,19 @@ AgentMain::~AgentMain( )
 
 int AgentMain::Run( )
 {
-	app_->signal_startup( ).connect(
-		sigc::mem_fun( *this, &AgentMain::on_startup ) );
-
 	return app_->run( );
 }
 
 void AgentMain::on_startup( )
 {
-	
+	app_->add_window( agent_window_ );
+}
+
+bool AgentMain::on_delete_event(
+	GdkEventAny*
+	)
+{
+	/// 종료 명령 무시
+	return true;
 }
 
