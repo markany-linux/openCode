910da4801bd6577f7c5e0d13676f75bfad35f209
MaBling <swma@markany.com>
2020-09-02 15:24:51 +0900

    * user/ 1. agent_main.cc/h  [ + ] 각종 UI를 담당하기 위한 컴포넌트 클래스 설계
    2. main.cc
     [ + ] 구조 리팩터링 기초 작업

---
+++ user/src/agent_main.cc
@@ -0,0 +1,18 @@
+#include "agent_main.h"
+
+AgentMain::AgentMain( )
+{
+}
+
+AgentMain::~AgentMain( )
+{
+}
+
+int AgentMain::Run( )
+{
+	app_->signal_startup( ).connect(
+		sigc::mem_fun( *this, &AgentMain::on_startup ) );
+
+	return app_->run( );
+}
+
