5b950f5ee82de70ef6ec5de46904b5a33b7a73a8
MaBling <swma@markany.com>
2020-09-02 17:39:36 +0900

    * user/ 1. button_box.cc/h  [ * ] 새 로직 추가를 위해 기존 로직 제거

--- user/src/button_box.cc
+++ user/src/button_box.cc
@@ -1,22 +1,15 @@
 #include "button_box.h"
-#include "contents_box.h"
 
 #include <iostream>
 
+#include "contents_box.h"
+
 ButtonBox::ButtonBox(
-	ContentsBox* contents_box__,
-	const std::vector<ButtonInfo>& info_list__
+	ContentsBox* contents_box__
 	)
 		: Gtk::Box( Gtk::ORIENTATION_VERTICAL, 0 ),
-		  buttons_( ),
 		  contents_box_( contents_box__)
 {
-	std::cout << "[+] ButtonBox::ButtonBox()\n";
-	/// 버튼 생성 정보가 하나라도 있다면
-	if( 0 < info_list__.size( ) )
-		AddButtons( info_list__ ); /// 실제 GTK Button 객체를 생성 후 벡터에 push
-	
-	std::cout << "[-] ButtonBox::ButtonBox()\n";
 }
 
 ButtonBox::~ButtonBox( )
@@ -24,38 +17,10 @@ ButtonBox::~ButtonBox( )
 	std::cout << "ButtonBox::~ButtonBox()\n";
 }
 
-void ButtonBox::AddButtons(
-	const std::vector<ButtonInfo>& info_list__
+void ButtonBox::on_button_clicked(
+	Gtk::Button* button__
 	)
-{
-	std::cout << "[+] ButtonBox::AddButtons()\n";
-	std::unique_ptr<Gtk::Button> button = nullptr;
-	
-	for( const auto& info : info_list__ )
-	{
-		/// GTK 버튼 객체 생성
-		button = std::make_unique<Gtk::Button>( info.text );
-		if( nullptr == button )
-			continue;
-		
-		/// Box에 button을 추가
-		pack_start( *button, Gtk::PACK_EXPAND_PADDING );
-		button->signal_clicked()
-				.connect(
-					sigc::bind(
-						sigc::mem_fun( *this,
-										 &ButtonBox::on_button_clicked ),
-						button.get() ) );
-		
-		/// 생성된 실제 Button 객체를 벡터에 push
-		buttons_.emplace_back( std::move( button ) );
-		button.reset();
-	}
-
-	std::cout << "[-] ButtonBox::AddButtons()\n";
-}
-
-void ButtonBox::on_button_clicked( Gtk::Button* button__ )
 {
 	contents_box_->text_window_.AddText( button__->get_label() );
 }
+
