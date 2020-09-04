3efd9482e2f2aabb11f9d503e9e3ac88ec210a99
MaBling <swma@markany.com>
2020-09-04 09:47:42 +0900

    * user/ 1. compile_flags.txt, Makefile  [ + ] lib_utility를 사용하기 위한 path 추가
    2. agent_data.cc/h
     [ + ] lib_utility로 부터 config 값을 얻기 위한 사전 작업

--- user/src/agent_data.cc
+++ user/src/agent_data.cc
@@ -1,12 +1,24 @@
 #include "agent_data.h"
 
+#include "lib_utility/gpl_list.h"
+#include "lib_utility/interface/config_handler.h"
+
 #include <string>
 
 AgentData::AgentData(
-	AgentMain*					agent_main__
+	AgentMain*					agent_main__,
+	std::string&				config_file_path__
 	) : agent_main_( agent_main__ )
 {
+	initConfigList( &config_list_, config_file_path__.c_str( ) );
 }
 
+void AgentData::GetConfigData( )
+{
+	PAGENT_CONFIG config = nullptr;
 
+	list_for_each( config, config_list_.list_head )
+	{
+	}
+}
 
