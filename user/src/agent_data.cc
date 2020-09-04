ea199076a3b5ac49397ec8a405e7412de1e2d071
MaBling <swma@markany.com>
2020-09-04 17:57:37 +0900

    * user/ 1. agent_data.cc/h  [ + ] 설정 파일을 읽어와서 문자열로 가공해주는 기능 구현

--- user/src/agent_data.cc
+++ user/src/agent_data.cc
@@ -7,18 +7,46 @@
 
 AgentData::AgentData(
 	AgentMain*					agent_main__,
-	std::string&				config_file_path__
+	std::string&				config_file_path__,
+	const ConfigKeyList&		key_list__
 	) : agent_main_( agent_main__ )
 {
 	initConfigList( &config_list_, config_file_path__.c_str( ) );
+	GetConfigData( key_list__ );
 }
 
-void AgentData::GetConfigData( )
+const DataString AgentData::GetConfigData(
+	const ConfigKeyList&		key_list__
+	)
 {
-	PAGENT_CONFIG config = nullptr;
+	char value[ kConfigValueSize ] = { 0, };
+	bool has_value = false;
+	DataString output = std::make_shared< std::string >( );
 
-	list_for_each( config, config_list_.list_head )
+	for( auto& key : key_list__ )
 	{
+		/// 문자열 항상 초기화
+		value[ 0 ] = '\0';
+		
+		/// 키를 통하여 값을 요청
+		has_value = getConfigListValue(
+						&config_list_, key.c_str( ), value, sizeof( value ) );
+		/// 해당 키를 설정에서 찾을 수 없거나 value 배열 크기가 모자란 경우
+		if( mild_false == has_value )
+			continue;
+
+		/// 문자열이 버퍼를 넘어서지 못하도록 배열 끝을 항상 끊음
+		value[ kConfigValueSize - 1 ] = '\0';
+
+		/// 출력 형식:
+		/// SERVER_IP = 192.168.1.1
+		/// PORT = 7777
+		output->append( key );
+		output->append( " = " );
+		output->append( value );
+		output->append( "\n" );
 	}
+
+	return output;
 }
 
--- user/src/agent_data.h
+++ user/src/agent_data.h
@@ -3,11 +3,13 @@
 
 #include <memory>
 #include <string>
+#include <vector>
 
 #include "lib_utility/interface/config_handler.h"
 
 class AgentMain;
 
+using ConfigKeyList = std::vector< std::string >;
 using DataString = std::shared_ptr< std::string >;
 
 class AgentData
@@ -15,10 +17,15 @@ class AgentData
 public:
 	/**
 	 * @brief	에이전트에서 각종 데이터를 가져오는 객체 생성
+	 *
+	 * @param	agent_main__		에이전트 메인 객체
+	 * @param	config_file_path__	설정 파일 경로
+	 * @param	key_list__			설정 파일에서 읽어올 키 리스트
 	 **/
 	AgentData(
 		AgentMain*				agent_main__,
-		std::string&			config_file_path__
+		std::string&			config_file_path__,
+		const ConfigKeyList&	key_list__
 		);
 
 	AgentData(
@@ -40,9 +47,21 @@ public:
 	 **/
 	~AgentData( );
 
-	DataString GetConfigData( );
+	/**
+	 * @brief	설정 파일의 정보를 가져와서 문자열로 변환
+	 *
+	 * @param	key_list__	키를 담은 리스트
+	 * 
+	 * @return	설정 파일에 있는 정보들을 읽을 수 있도록 변환된 문자열
+	 **/
+	const DataString GetConfigData(
+		const ConfigKeyList&	key_list__
+		);
 
 private:
+	/// 설정 파일에서 특정 키에 대한 값을 읽을 때 값의 최대 길이 지정
+	static constexpr int kConfigValueSize = 256;
+
 	AgentMain* agent_main_ = nullptr;
 	CONFIG_LIST config_list_;
 };
