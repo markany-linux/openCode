16e9c1c0e71e2b944b02ee4ed0ed8fc190b5dd01
MaBling <swma@markany.com>
2020-09-04 09:16:49 +0900

    * user/ 1. agent_data.cc/h  [ + ] lib_utility로 부터 받은 데이터를 가공해주는 클래스 추가

---
+++ user/src/agent_data.cc
@@ -0,0 +1,12 @@
+#include "agent_data.h"
+
+#include <string>
+
+AgentData::AgentData(
+	AgentMain*					agent_main__
+	) : agent_main_( agent_main__ )
+{
+}
+
+
+
---
+++ user/src/agent_data.h
@@ -0,0 +1,47 @@
+#ifndef __AGENT_DATA_H__
+#define __AGENT_DATA_H__
+
+#include <memory>
+#include <string>
+
+class AgentMain;
+
+using DataString = std::shared_ptr< std::string >;
+
+class AgentData
+{
+public:
+	/**
+	 * @brief	에이전트에서 각종 데이터를 가져오는 객체 생성
+	 **/
+	AgentData(
+		AgentMain* agent_main__
+		);
+
+	AgentData(
+		const AgentData&
+		) = delete;
+	AgentData& operator=(
+		const AgentData&
+		) = delete;
+
+	AgentData(
+		AgentData&&
+		) = delete;
+	AgentData& operator=(
+		AgentData&&
+		) = delete;
+
+	/**
+	 * @brief	에이전트 객체 소멸
+	 **/
+	~AgentData( );
+
+	DataString GetConfigData( );
+
+private:
+	AgentMain* agent_main_ = nullptr;
+};
+
+#endif
+
