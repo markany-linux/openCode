d517d776f3d32e0ca7a3aa8ec7ea925fe9520bcb
MaBling <swma@markany.com>
2020-09-11 16:28:14 +0900

    * user/ 1. agent_main.cc/h  [ + ] 프로그램이 중복으로 실행되지 못하도록 수정
    2. main.cc
     [ + ] 1에 대한 수정

--- user/src/agent_main.cc
+++ user/src/agent_main.cc
@@ -1,25 +1,22 @@
 #include "agent_main.h"
 
+#include <cassert>
+#include <fstream>
 #include <iostream>
+#include <sys/stat.h>
 
+#include "agent_data.h"
 #include "button_box.h"
+#include "lib_utility/interface/single_instance.h"
 
 const std::string AgentMain::kConfigFilePath = "config.cfg";
 
-AgentMain::AgentMain( )
-{
-	/// 애플리케이션이 구동을 시작할 때에 대한 핸들러 등록
-	app_->signal_startup( ).connect(
-		sigc::mem_fun( *this, &AgentMain::on_startup ) );
-
-	/// 사용자가 임의로 종료를 시도할 때에 대한 핸들러 등록
-	agent_window_.signal_delete_event( )
-		.connect( sigc::mem_fun( *this, &AgentMain::on_delete_event ) );
-}
-
 AgentMain::~AgentMain( )
 {
+	std::cout << "[+] AgentMain::~AgentMain()\n";
 	app_->quit( );
+	cleanupSingleInstance( &instance_, instance_path_.c_str( ) );
+	std::cout << "[-] AgentMain::~AgentMain()\n";
 }
 
 int AgentMain::Run( )
@@ -28,6 +25,36 @@ int AgentMain::Run( )
 	int ret = app_->run( );
 	std::cout << "[-] AppMain::Run()\n";
 	return ret;
+}
+
+bool AgentMain::Init(
+	const char*					instance_path__
+	)
+{
+	assert( instance_path__ && "Invalid instance path" );
+
+	/// 중복 실행을 방지하기 위한 싱글 인스턴스 설정
+	bool instance_setup = setupSingleInstance( &instance_, instance_path__ );
+	if( !instance_setup )
+		return false;
+	
+	/// 프로그램 종료 시 싱글 인스턴스를 해제하기 위해 경로 저장
+	instance_path_ = instance_path__;
+	
+	/// 애플리케이션이 구동을 시작할 때에 대한 핸들러 등록
+	app_->signal_startup( ).connect(
+		sigc::mem_fun( *this, &AgentMain::on_startup ) );
+
+	/// 사용자가 임의로 종료를 시도할 때에 대한 핸들러 등록
+	agent_window_.signal_delete_event( )
+		.connect( sigc::mem_fun( *this, &AgentMain::on_delete_event ) );
+	
+	return true;
+}
+
+bool AgentMain::Init( )
+{
+	return Init( kDefaultInstancePath );
 }
 
 void AgentMain::on_button_clicked(
@@ -63,8 +90,11 @@ void AgentMain::on_button_clicked(
 void AgentMain::on_startup( )
 {
 	std::cout << "[+] AgentMain::on_startup()\n";
-	app_->add_window( agent_window_ );
-	agent_window_.show( );
+	if( instance_ )
+	{
+		app_->add_window( agent_window_ );
+		agent_window_.show( );
+	}
 	std::cout << "[-] AgentMain::on_startup()\n";
 }
 
