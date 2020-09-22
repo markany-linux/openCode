0a8c9683c90f0cc42ca7bf6be5972917a522f376
wikim <wikim@markany.com>
2020-09-23 08:32:46 +0900

    1. mod_main.c  [ + ] netlink 초기화 정보를 획득하여 sysfs로 전달하도록 코드 추가  [ + ] 각 함수 단위별 주석 추가

--- kernel/mod_main.c
+++ kernel/mod_main.c
@@ -15,18 +15,23 @@
  */
 static int __init hello_markany_openCode( void )
 {
-    printk( "%s: Hello Kernel!\n", __FUNCTION__ );
+    mild_bool netlink_load = mild_false;
 
-    if( mild_false == initNetLinkSocket( ) )
+    printk( "%s: Hello Kernel! This is openCode Kernel Module\n", __FUNCTION__ );
+
+    /// netlink 커널 객체 생성 및 결과 획득
+    netlink_laod = initNetLinkSocket( );
+    /// netlink 생성 실패 확인
+    if( mild_false == netlink_load )
     {
         printk( "Fail to initialize netlink\n" );
-        return -1;
     }
 
-    /// sysfs 객체 초기화
-    if( mild_false == initSysfsKernelObject( mild_true ) )
+    /// sysfs 객체 초기화 및 netlink 결과 전달
+    if( mild_false == initSysfsKernelObject( netlink_load ) )
     {
-        printk( "Fail to initialize sysfs kernel object\n" );
+        /// sysfs 객체 생성 실패 시, 커널 모듈을 적재하지 않도록 구성
+        printk( "Fail to initialize sysfs kernel object\nFail to load openCode Kernel Module\n\n" );
         return -1;
     }
 
@@ -37,18 +42,19 @@ static int __init hello_markany_openCode( void )
 /**
  * @brief   커널 모듈 정리 함수
  *          rmmod 명령으로 제거될 때 호출
- * 
  */
 static void __exit bye_markany_openCode( void )
 {
+    /// netlink 해제
     exitNetLinkSocket( );
 
     /// sysfs 객체 해제
     exitSysfsKernelObject( );
 
+    /// 존재할 지 모르는 접근 리스트를 모두 해제
     cleanupAccessList( );
 
-    printk( "%s: Bye Kernel\n", __FUNCTION__ );
+    printk( "%s: Bye Kernel...\n", __FUNCTION__ );
 }
 
 
